b9a4069dcf76b360c208b7e05d290edfc99aa78b
Ryan <kekemusa37@nate.com>
2020-04-05 23:27:14 +0900

    Update 05. Best Time to Buy and Sell Stock II.kt

--- 30 Day Challenge/week 1/05. Best Time to Buy and Sell Stock II.kt
+++ 30 Day Challenge/week 1/05. Best Time to Buy and Sell Stock II.kt
@@ -1,19 +1,11 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        var (index, result) = 0 to 0
-        var min: Int
-        var max: Int
+        var result = 0
 
-        while (index < prices.size - 1) {
-            while (index < prices.size - 1 && prices[index] >= prices[index + 1]) {
-                index++
+        for (i in 0 until prices.size - 1) {
+            if (prices[i + 1] > prices[i]) {
+                result += prices[i + 1] - prices[i]
             }
-            min = prices[index]
-            while (index < prices.size - 1 && prices[index] <= prices[index + 1]) {
-                index++
-            }
-            max = prices[index]
-            result += max - min
         }
         return result
     }
