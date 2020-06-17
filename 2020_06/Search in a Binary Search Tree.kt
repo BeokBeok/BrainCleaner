bda722df27936d0017ec2628685e138f977a7482
Ryan <kekemusa37@nate.com>
2020-06-17 14:59:42 +0900

    700. Search in a Binary Search Tree
    https://leetcode.com/problems/search-in-a-binary-search-tree/

---
+++ 2020_06/Search in a Binary Search Tree.kt
@@ -0,0 +1,21 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) return null
+
+        return when {
+            root.`val` > `val` -> searchBST(root.left, `val`)
+            root.`val` < `val` -> searchBST(root.right, `val`)
+            else -> root
+        }
+    }
+}
